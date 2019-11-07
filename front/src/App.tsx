cf4140f49b47cf409db13d24f8c2e4e0fa424ab5
Anish Duwal <duwalanise@gmail.com>
2019-11-07 20:24:11 +0545

    responsive layout

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,25 +1,62 @@
-import * as React from "react";
-import GridLayout from "react-grid-layout";
+import * as React from 'react';
+import { Responsive, WidthProvider } from 'react-grid-layout';
 
-class App extends React.Component {
-  public render() {
+const ResponsiveGridLayout = WidthProvider(Responsive);
+
+class App extends React.Component<any, any> {
+  public state = {
+    editMode: false,
+  };
+
+  public onChangeLayout = (layout) => {
+    console.log(layout);
+  };
+
+  public getLayout() {
     const layout = [
-      { i: "a", x: 0, y: 0, w: 1, h: 2, static: true },
-      { i: "b", x: 1, y: 0, w: 3, h: 2, minW: 2, maxW: 4 },
-      { i: "c", x: 4, y: 0, w: 1, h: 2 }
+      { i: 'b', x: 1, y: 0, w: 2, h: 1 },
+      { i: 'a', x: 4, y: 0, w: 2, h: 1 },
+      { i: 'c', x: 7, y: 0, w: 2, h: 1 },
     ];
+    return {
+      lg: layout,
+      md: layout,
+      sm: layout,
+      xs: layout,
+      xxs: layout,
+    };
+  }
+
+  public changeMode = () => {
+    this.setState({ editMode: !this.state.editMode });
+  };
+
+  public render() {
     return (
-      <GridLayout
-        className="layout"
-        layout={layout}
-        cols={12}
-        rowHeight={30}
-        width={1200}
-      >
-        <div key="a">a</div>
-        <div key="b">b</div>
-        <div key="c">c</div>
-      </GridLayout>
+      <div>
+        <ResponsiveGridLayout
+          breakpoints={{ lg: 1200, md: 996, sm: 768, xs: 480, xxs: 0 }}
+          cols={{ lg: 12, md: 10, sm: 6, xs: 4, xxs: 2 }}
+          className="layout"
+          layouts={this.getLayout()}
+          rowHeight={30}
+          onLayoutChange={this.onChangeLayout}
+          draggableHandle=".drag-me"
+        >
+          <div key="a">
+            <textarea>Hello A</textarea>
+            <span className="drag-me">D</span>
+          </div>
+          <div key="b">
+            <textarea>Hello B</textarea>
+            <span className="drag-me">D</span>
+          </div>
+          <div key="c">
+            <textarea>Hello C</textarea>
+            <span className="drag-me">D</span>
+          </div>
+        </ResponsiveGridLayout>
+      </div>
     );
   }
 }
